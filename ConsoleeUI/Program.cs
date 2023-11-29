e4d24f24a9178546cb8df6e8eb2e19c224f4cde9
Ruken Afşin <rukenafsin13@gmail.com>
2023-11-29 15:13:03 +0300

    RentACar_N-Tier-Layer

---
+++ ConsoleeUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ConsoleeUI
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+           foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+                Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfBrandDal : IBrandDal
+    {
+        public void Add(Brand t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Brand t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Brand Get(Expression<Func<Brand, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Brand> GetAll(Expression<Func<Brand, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Brand t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
